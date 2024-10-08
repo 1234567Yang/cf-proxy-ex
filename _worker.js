a7569f1633e97a84fcdbaa803a460d2716e11d17
Caecilia <caecilia@example.com>
2024-10-08 18:11:37 +0800

    按要求重写mainPage

--- _worker.js
+++ _worker.js
@@ -27,7 +27,7 @@ setTimeout(() => {
     document.body.insertAdjacentHTML(
       'afterbegin', 
       \`<div style="position:fixed;left:0px;top:0px;width:100%;margin:0px;padding:0px;z-index:9999999999999999999;user-select:none;cursor:pointer;" id="__PROXY_HINT_DIV__" onclick="document.getElementById('__PROXY_HINT_DIV__').remove();">
-        <span style="position:absolute;width:100%;min-height:30px;font-size:20px;color:white;background:grey;text-align:center;border-radius:5px;">
+        <span style="position:absolute;width:100%;min-height:30px;font-size:20px;color:yellow;background:red;text-align:center;border-radius:5px;">
           \${hint}
         </span>
       </div>\`    
@@ -512,88 +512,95 @@ const mainPage = `
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
-<meta charset="UTF-8">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet">
-<title>Proxy Everything</title>
-<link rel="icon" type="image/png" href="https://img.icons8.com/color/1000/kawaii-bread-1.png">
-<meta name="Description" content="Proxy Everything with CF Workers.">
-<meta property="og:description" content="Proxy Everything with CF Workers.">
-<meta property="og:image" content="https://img.icons8.com/color/1000/kawaii-bread-1.png">
-<meta name="robots" content="index, follow">
-<meta http-equiv="Content-Language" content="zh-CN">
-<meta name="copyright" content="Copyright © ymyuuu">
-<meta name="author" content="ymyuuu">
-<link rel="apple-touch-icon-precomposed" sizes="120x120" href="https://img.icons8.com/color/1000/kawaii-bread-1.png">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
-<style>
-    body, html {
-        height: 100%;
-        margin: 0;
+  <meta charset="UTF-8">
+  <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body{
+      background:rgb(150,10,10);
+      color:rgb(240,240,0);
     }
-    .background {
-        background-image: url('https://imgapi.cn/bing.php');
-        background-size: cover;
-        background-position: center;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+    a{
+      color:rgb(250,250,180);
     }
-    .card {
-        background-color: rgba(255, 255, 255, 0.8);
-        transition: background-color 0.3s ease, box-shadow 0.3s ease;
+    del{
+      color:rgb(190,190,190);
     }
-    .card:hover {
-        background-color: rgba(255, 255, 255, 1);
-        box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.3);
+    .center{
+      text-align:center;
     }
-    .input-field input[type=text] {
-        color: #2c3e50;
+    .important{
+      font-weight:bold;
+      font-size:27;
     }
-    .input-field input[type=text]:focus+label {
-        color: #2c3e50 !important;
-    }
-    .input-field input[type=text]:focus {
-        border-bottom: 1px solid #2c3e50 !important;
-        box-shadow: 0 1px 0 0 #2c3e50 !important;
-    }
-</style>
+    /* my style begins*/
+    form[id=urlForm] {
+        max-width: 340px;
+        min-width: 340px;
+        margin: 0 auto;
+     }
+    input[id=targetUrl] {
+        background-color: rgb(240,240,0);
+     }
+    button[id=jumpButton] {
+        background-color: rgb(240,240,0);
+     }
+  </style>
 </head>
 <body>
-<div class="background">
-    <div class="container">
-        <div class="row">
-            <div class="col s12 m8 offset-m2 l6 offset-l3">
-                <div class="card">
-                    <div class="card-content">
-                        <span class="card-title center-align"><i class="material-icons left">link</i>Proxy Everything</span>
-                        <form id="urlForm" onsubmit="redirectToProxy(event)">
-                            <div class="input-field">
-                                <input type="text" id="targetUrl" placeholder="在此输入目标地址" required>
-                                <label for="targetUrl">目标地址</label>
-                                <input type="password" id="accessToken" placeholder="在此输入访问密码">
-                            </div>
-                            <button type="submit" class="btn waves-effect waves-light teal darken-2 full-width">跳转</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-<script>
-    function redirectToProxy(event) {
-        event.preventDefault();
-        const targetUrl = document.getElementById('targetUrl').value.trim();
-        const accessToken = document.getElementById('accessToken').value.trim();
-        const currentOrigin = window.location.origin;
-        document.cookie += "__PROXY_PWD__=" + accessToken + "; path=/; domain=.domain.ltd";
-        window.open(currentOrigin + '/' + targetUrl, '_blank');
-    }
-</script>
+    <h3 class="center">
+        I made this project because some extreme annoying network filter software in my school, which is notorious "Goguardian", and now it is open source at <a>https://github.com/1234567Yang/cf-proxy-ex/</a>.
+      </h3>
+      <br><br><br>
+      <ul style="font-size:25;">
+      <li class="important">How to use this proxy:<br>
+        Type the website you want to go to after the website's url, for example: <br>
+        https://the current url/github.com<br>OR<br>https://the current url/https://github.com</li>
+      </ul>
+        <form id="urlForm" onsubmit="redirectToProxy(event)">
+            <fieldset>
+                <legend>Proxy Everything</legend>
+                <label for="targetUrl">TargetUrl: <input type="text" id="targetUrl" placeholder="Enter the target URL here..."></label>
+                <button type="submit" id="jumpButton">Jump!</button>
+            </fieldset>
+        </form>
+        <script>
+            function redirectToProxy(event) {
+                event.preventDefault();
+                const targetUrl = document.getElementById('targetUrl').value.trim();
+                const currentOrigin = window.location.origin;
+                window.open(currentOrigin + '/' + targetUrl, '_blank');
+            }
+        </script>
+      <ul>
+        <li>If your browser show 400 bad request, please clear your browser cookie<br></li>
+        <li>Why I make this:<br> Because school blcok every website that I can find math / CS and other subjects' study material and question solutions. In the eyes of the school, China (and some other countries) seems to be outside the scope of this "world". They block access to server IP addresses in China and block access to Chinese search engines and video websites. Of course, some commonly used social software has also been blocked, which once made it impossible for me to send messages to my parents on campus. I don't think that's how it should be, so I'm going to fight it as hard as I can. I believe this will not only benefit myself, but a lot more people can get benefits.</li>
+       <li>If this website is blocked by your school: <br>Contact me at <a href="mailto:help@wvusd.homes">help@wvusd.homes</a>, and I will setup a new webpage.</li>
+        <li>Limitation:<br>Although I tried my best to make every website proxiable, there still might be pages or resources that can not be load, and the most important part is that <span class="important">YOU SHOULD NEVER LOGIN ANY ACCOUNT VIA ONLINE PROXY</span>.</li>
+      </ul>
+
+    <h3>
+        <br>
+        <span>Proxies that can bypass the school network blockade:</span>
+        <br><br>
+        <span>Traditional VPNs such as <a href="https://hide.me/">hide me</a>.</span>
+        <br><br>
+        <a href="https://www.torproject.org/">Tor Browser</a><span>, short for The Onion Router, is free and open-source software for enabling anonymous communication. It directs Internet traffic via a free, worldwide volunteer overlay network that consists of more than seven thousand relays. Using Tor makes it more difficult to trace a user's Internet activity.</span>
+        <br><br>
+        <a href="https://v2rayn.org/">v2RayN</a><span> is a GUI client for Windows, support Xray core and v2fly core and others. You must subscribe to an <a href = "https://aijichang.org/6190/">airport</a> to use it. For example, you can subscribe <a href="https://feiniaoyun.xyz/">fly bird cloud</a>.</span>
+        <br><br>
+        <span>Bypass <del>Goguardian</del> by proxy: You can buy a domain($1) and setup by yourself: </span><a href="https://github.com/gaboolic/cloudflare-reverse-proxy">how to setup a proxy</a><span>. Unless <del>Goguardian</del> use white list mode, this can always work.</span>
+        <br>
+        <span>Too expensive? Never mind! There are a lot of free domains registration companies (for the first year of the domain) that do not need any credit card, search online!</span>
+        <br><br>
+        <span>Youtube video unblock: "Thanks" for Russia that they started to invade Ukraine and Google blocked the traffic from Russia, there are a LOT of mirror sites working. You can even <a href="https://github.com/iv-org/invidious">setup</a> one by yourself.</span>
+    </h3>
+    <a href="https://goguardian.com" style="visibility:hidden"></a>
+    <a href="https://blocked.goguardian.com/" style="visibility:hidden"></a>
+    <a href="https://www.google.com/gen_204" style="visibility:hidden"></a>
+    <p style="font-size:280px !important;width:100%;" class="center">
+        ☭
+    </p>
 </body>
 </html>
 `;
@@ -607,17 +614,7 @@ async function handleRequest(request) {
   //获取所有cookie
   var siteCookie = request.headers.get('Cookie');
 
-  const url = new URL(request.url);
-  if(request.url.endsWith("favicon.ico")){
-    return Response.redirect("https://www.baidu.com/favicon.ico", 301);
-  }
-  //var siteOnly = url.pathname.substring(url.pathname.indexOf(str) + str.length);
-
-  var actualUrlStr = url.pathname.substring(url.pathname.indexOf(str) + str.length) + url.search + url.hash;
-  if (actualUrlStr == "") { //先返回引导界面
-    return getHTMLResponse(mainPage);
-  }
-
+  
   if (password != "") {
     if(siteCookie != null && siteCookie != ""){
       var pwd = getCook(passwordCookieName, siteCookie);
@@ -634,6 +631,18 @@ async function handleRequest(request) {
     }
 
   }
+
+  const url = new URL(request.url);
+  if(request.url.endsWith("favicon.ico")){
+    return Response.redirect("https://www.baidu.com/favicon.ico", 301);
+  }
+  //var siteOnly = url.pathname.substring(url.pathname.indexOf(str) + str.length);
+
+  var actualUrlStr = url.pathname.substring(url.pathname.indexOf(str) + str.length) + url.search + url.hash;
+  if (actualUrlStr == "") { //先返回引导界面
+    return getHTMLResponse(mainPage);
+  }
+
 
   try {
     var test = actualUrlStr;
