c3015c53839ba1e2b86569df30ce76f02c6c445f
Yang <113082249+1234567Yang@users.noreply.github.com>
2025-05-07 10:04:57 -0700

    代理地址host如果是大写就转小写
    ExAMple.com -> redirect to example.com

--- _worker.js
+++ _worker.js
@@ -577,8 +577,8 @@ console.log("WINDOW CORS ERROR EVENT ADDED");
 
 
 `;
-httpRequestInjection = 
-`(function () {`
+httpRequestInjection =
+  `(function () {`
   + httpRequestInjection +
   `})();`;
 const mainPage = `
@@ -744,7 +744,7 @@ async function handleRequest(request) {
       headers: { "Content-Type": "text/plain" },
     });
   }
-  
+
   //var siteOnly = url.pathname.substring(url.pathname.indexOf(str) + str.length);
 
   var actualUrlStr = url.pathname.substring(url.pathname.indexOf(str) + str.length) + url.search + url.hash;
@@ -782,8 +782,40 @@ async function handleRequest(request) {
     //actualUrlStr = "https://" + actualUrlStr;
     return Response.redirect(thisProxyServerUrlHttps + "https://" + actualUrlStr, 301);
   }
+
   //if(!actualUrlStr.endsWith("/")) actualUrlStr += "/";
   const actualUrl = new URL(actualUrlStr);
+
+
+
+
+
+  //check for upper case: proxy.com/https://ABCabc.dev
+  {
+    var checkHostCase = actualUrlStr.substring(actualUrlStr.indexOf("://") + 3);
+
+    var pos1 = checkHostCase.indexOf("\\");
+    var pos2 = checkHostCase.indexOf("/");
+    var finalPos;
+    if (pos1 === -1 && pos2 === -1) {
+      finalPos = -1; // 都没有找到
+    } else if (pos1 === -1) {
+      finalPos = pos2;
+    } else if (pos2 === -1) {
+      finalPos = pos1;
+    } else {
+      finalPos = Math.min(pos1, pos2);
+    }
+
+
+    checkHostCase = checkHostCase.substring(0, (finalPos != -1) ? finalPos : checkHostCase.length);
+
+    if (checkHostCase.toLowerCase() != checkHostCase) {
+      //actualUrl.href 会自动转换host为小写
+      return Response.redirect(thisProxyServerUrlHttps + actualUrl.href, 301);
+    }
+  }
+
 
   let clientHeaderWithChange = new Headers();
   //***代理发送数据的Header：修改部分header防止403 forbidden，要先修改，   因为添加Request之后header是只读的（***ChatGPT，未测试）
@@ -1096,4 +1128,13 @@ function getHTMLResponse(html) {
       "Content-Type": "text/html; charset=utf-8"
     }
   });
+}
+// https://stackoverflow.com/questions/14480345/how-to-get-the-nth-occurrence-in-a-string
+function nthIndex(str, pat, n) {
+  var L = str.length, i = -1;
+  while (n-- && i++ < L) {
+    i = str.indexOf(pat, i);
+    if (i < 0) break;
+  }
+  return i;
 }
