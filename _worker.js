40dca72aa8278fac75811befebb0c92e84307d59
keney2023 <134574829+keney2023@users.noreply.github.com>
2026-01-09 17:19:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ const str = "/";
 const lastVisitProxyCookie = "__PROXY_VISITEDSITE__";
 const passwordCookieName = "__PROXY_PWD__";
 const proxyHintCookieName = "__PROXY_HINT__";
-const password = "";
+const password = "130663";
 const showPasswordPage = true;
 const replaceUrlObj = "__location__yproxy__";
 
